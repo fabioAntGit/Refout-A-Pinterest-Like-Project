808c12970daad1dbd5376e9416b2442f9671360d
Fábio André Antunes <fabio.antunes1414@gmail.com>
2023-06-11 18:48:56 +0100

    email v27

--- app/urls.py
+++ app/urls.py
@@ -31,11 +31,6 @@ urlpatterns = [
     path('change_password/', views.change_password, name='change_password'),
     path('save_changes/<uuid:post_id>/', views.save_changes, name='save_changes'),
 
-    path('reset_password/',auth_views.PasswordResetView.as_view(),name='reset_password'),
-    path('reset_password_sent/',auth_views.PasswordResetDoneView.as_view(),name='password_reset_done'),
-    path('reset/<uidb64>/<token>',auth_views.PasswordResetConfirmView.as_view(),name='password_reset_confirm'),
-    path('reset_password_complete/',auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
-
 
 
     
