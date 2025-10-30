f3b9a746152de7d8c664645b9128938000107066
fabioAntGit <107940399+fabioAntGit@users.noreply.github.com>
2025-10-30 01:23:16 +0000

    Update settings.py

--- refout/settings.py
+++ refout/settings.py
@@ -149,8 +149,8 @@ GRAPH_MODELS = {
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'refoutapp@gmail.com'
-EMAIL_HOST_PASSWORD = 'qdztsglakvwtuocb'
+EMAIL_HOST_USER = '@'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
